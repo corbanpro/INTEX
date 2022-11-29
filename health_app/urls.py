1e52794b4f9cf8f197347c3a177115ecdc68a115
ckearl <c2_kearl@yahoo.com>
2022-11-29 15:54:42 -0700

    urls update

--- health_app/urls.py
+++ health_app/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
-<<<<<<< HEAD
 from .views import indexPageView, dashboardPageView, historyPageView, dashboardRecipePageView, registerPageView, loginPageView, addRecipePageView, addIngredientPageView, dashboardIngredientPageView, dashboardIngredientUnitPageView
-=======
-from .views import indexPageView, dashboardPageView, historyPageView, dashboardRecipePageView, registerPageView, loginPageView, addRecipePageView,dashboardUserPageView
->>>>>>> d9d60ba (push)
 
 urlpatterns = [
     path("", indexPageView, name="index"), 
@@ -13,13 +9,9 @@ urlpatterns = [
     path('addRecipe/', addRecipePageView, name= 'addRecipe'),
     path('register/', registerPageView, name= 'register'),
     path('login/', loginPageView, name = 'login'),
-<<<<<<< HEAD
     path('dashIngredient/', dashboardIngredientPageView, name= 'dashIngredient'),
     path('ingredientUnit/', dashboardIngredientUnitPageView, name= 'ingredientUnit'),  
     path('addIngredient/<int:ingredient_id>', addIngredientPageView, name= 'addIngredient'),
 
 
-=======
-    path('addUser', dashboardUserPageView, name='dashUser')
->>>>>>> d9d60ba (push)
 ]                  
