31503a547692a8a3c116dee1be1f30d279cafa2b
ckearl <c2_kearl@yahoo.com>
2022-11-29 01:58:22 -0700

    updates to baset and static js files

--- health_app/views.py
+++ health_app/views.py
@@ -3,13 +3,13 @@ from .functions import searchRecipes
 
 
 def indexPageView(request) :
-    recipe_name = request.GET['recipe_name']
-    returned_recipes = searchRecipes(recipe_name)    
-    context = {
-            'recipes' : returned_recipes['Chicken Quesadilla']
-        }
+    # recipe_name = request.GET['recipe_name']
+    # returned_recipes = searchRecipes(recipe_name)    
+    # context = {
+    #         'recipes' : returned_recipes['Chicken Quesadilla']
+    #     }
 
-    return render(request, 'health_app/index.html', context)
+    return render(request, 'health_app/index.html')
 
     # else: 
     #     return render(request, 'health_app/index.html')
