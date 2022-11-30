99f28b5f771f70e55cd185c2d968af31b175f9e5
McKenna Staley <mnnorton@byu.edu>
2022-11-30 15:19:53 -0700

    changed the units on select

--- health_app/urls.py
+++ health_app/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('addIngredient/<int:ingredient_id><int:user_id><str:ingredient_name>', addIngredientPageView, name= 'addIngredient'),
     path('addWater/<int:user_id>', addWaterPageView, name= 'addWater'),
     path('dashLogin/', dashboardLoginPageView, name= 'dashLogin'),
-    path('dashLogin/', dashboardPageView, name= 'pickNut'),
+    path('dashPickNut/', dashboardPageView, name= 'pickNut'),
     path('update/', updateRecipe, name='update_recipe'),
     # path('dashPie/', pieChart, name='pieChart' )
 
