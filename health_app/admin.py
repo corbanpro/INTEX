e29f02138c861dcbaefb8fa3bd686174d01298ba
corbanpro <corbanpro@gmail.com>
2022-11-30 12:15:39 -0700

    dvdeterminates, create dvD, lots of models stuff

--- health_app/admin.py
+++ health_app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import User, Comorbidity, Meal, DailyValue, Unit, Recipe
+from .models import User, DvDeterminate, Meal, DailyValue, Unit, Recipe
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Comorbidity)
+admin.site.register(DvDeterminate)
 admin.site.register(Meal)
 admin.site.register(DailyValue)
 admin.site.register(Unit)
