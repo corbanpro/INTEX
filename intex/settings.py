cccd1fb5e9036513b7e3c78aa631e7a036f3ef46
McKenna Staley <mnnorton@byu.edu>
2022-11-30 16:14:05 -0700

    mckenna 66

--- intex/settings.py
+++ intex/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'intex_health_app',
         'USER' : 'postgres',
-        'PASSWORD' : 'Nbamwia2cf',
+        'PASSWORD' : 'Mn160132',
         'HOST' : 'localhost',
         'PORT' : '5432'
     }
